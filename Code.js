7c044ff079688be238ea7d637e8f6e1ae3904d18
Matt Crow <greengrappler12@gmail.com>
2021-06-05 13:05:19 -0700

    Wrote down a few ideas

--- Code.js
+++ Code.js
@@ -1,3 +1,40 @@
+/*
+The folder structure of a Google Drive is tree-like in nature, with folders
+(assuming they contain at least one file) as internal nodes, and files as leaf
+nodes. This script must process every descendent leaf node of the root nodes it
+is given.
+
+Google Script, however, adds a wrinkle into the standard tree-traversal strategy:
+Scripts are limited in how long they can run, usually around 5 minutes. Therefore,
+the script must have some way of remembering which folders it has already finished
+processing, while also remembering the child folders of those folders it has yet
+to process.
+
+There are multiple steps involved in processing a folder structure:
+1. get a root folder
+2. recur for each subfolder in that folder
+3. process all files in that folder
+4. mark the root folder as done
+Steps 2 & 3 are interchangable for either post-order or pre-order traversal.
+
+The problem with this is that Google Script will likely time out before step 4,
+and so it may never be marked complete for large folder structures.
+
+Here is the proposed alternative method:
+1. get a root folder from the queue
+2. push each subfolder to a processing stack, stored in a Google Sheet
+3. process all files in the folder
+4. mark the root folder as done, removing it from the queue
+5. pop the top of the processing stack, and move it to the start of the queue
+
+
+
+!!! HERE !!!
+Essentially, I need to figure out how to process the folders pre-order while also
+maintaining the indentation output given by the outputter and keeping in mind
+Google Script's limitations
+*/
+
 const PROCESSING_LIST_NAME = "folders to process";
 const EXTENSIONS = ["doc", "docx", "xls", "xlsx", "ppt", "pptx"].map((e)=>e.toLowerCase());
 
@@ -115,7 +152,7 @@ class FileProcessor {
       return;
     }
     let reformatted = null;
-    
+
     if(this.shouldProcess(file)){
       // do conversion
       reformatted = toGoogleFile(file);
